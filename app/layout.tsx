b8b64189a9d6ed473628a54df83e3d2ab6593473
Axit Undhad <axitundhad065@gmail.com>
2025-06-03 17:53:41 +0530

    Web Analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,11 @@ import Providers from "./components/Providers";
 import Header from "./components/Header";
 import Script from "next/script";
 import { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata : Metadata = {
+export const metadata: Metadata = {
   title: "AxitPix",
   description: "Online picture selling platform",
   // icons:{
@@ -22,7 +23,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      
       <body className={inter.className}>
         <Script
           src="https://checkout.razorpay.com/v1/checkout.js"
@@ -32,6 +32,7 @@ export default function RootLayout({
           <Header />
           <main className="container mx-auto px-4 pt-6 pb-8">{children}</main>
         </Providers>
+        <Analytics />
       </body>
     </html>
   );
